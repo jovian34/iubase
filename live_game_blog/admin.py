d59b0403202d7a375e5aea4038b6bce2c8fc5703
Carl James <carl@jovian34.com>
2023-12-10 18:15:59 -0500

    tweaked admin deleted all migrations

--- live_game_blog/admin.py
+++ live_game_blog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.db import models
+from .models import Game, GameBlogEntry, Team
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    model = Team
+    list_display = (
+        "team_name",
+        "mascot",
+        "logo",
+        "stats",
+        "roster",
+    )
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    model = Game
+    list_display = (
+        "home_team",
+        "away_team",
+        "neutral_site",
+        "live_stats",
+        "first_pitch",
+    )
+
+
+
+@admin.register(GameBlogEntry)
+class GameBlogEntryAdmin(admin.ModelAdmin):
+    model = GameBlogEntry
+    list_display = (
+        "game",
+        "blog_time",
+        "inning_num",
+        "inning_part",
+        "outs",
+    )

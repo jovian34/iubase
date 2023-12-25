c8b048bb824a66c5b20c76cdc4b9504df3dea974
Carl James <carl@jovian34.com>
2023-12-25 12:24:57 -0500

    added function to form quash Dj6.0 warning ATP

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django.db import models
 
+def urlfields_assume_https(db_field, **kwargs):
+    """
+    ModelForm.Meta.formfield_callback function to assume HTTPS for scheme-less
+    domains in URLFields.
+    """
+    if isinstance(db_field, models.URLField):
+        kwargs["assume_scheme"] = "https"
+    return db_field.formfield(**kwargs)
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("x_prof",)
-
+        formfield_callback = urlfields_assume_https
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = UserChangeForm.Meta.fields
+        formfield_callback = urlfields_assume_https
--- django_project/settings.py
+++ django_project/settings.py
@@ -140,5 +140,5 @@ if not bool(int(os.environ.get("DEVELOP"))):
     STATIC_ROOT = os.path.join(BASE_DIR, 'django_project/static/')
 
 
-project_version = "0.0.8" # iubase-logo added with bullets for apps 2023-12-21
+project_version = "0.0.9" # added function to form quash Dj6.0 warning ATP 2023-12-24
 os.environ.setdefault("PROJECT_VERSION", project_version)

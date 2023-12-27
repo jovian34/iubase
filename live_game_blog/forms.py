90f87fb150c283de7ad07f7cb480bb5d2a2c9207
Carl James <carl@jovian34.com>
2023-12-27 12:33:00 -0500

    save form choices

---
+++ live_game_blog/forms.py
@@ -0,0 +1,15 @@
+INNING_PART_CHOICES = [
+    ("pre-game", "game not started"),
+    ("top", "top of an inning"),
+    ("middle", "middle of an inning"),
+    ("bottom", "bottom of an inning"),
+    ("end", "end of an inning while game continues"),
+    ("final", "end of inning and game concluded")
+]
+
+OUTS_CHOICES = [
+    (0, "none"),
+    (1, "one"),
+    (2, "two"),
+    (3, "three"),
+]

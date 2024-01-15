04decbba6cb57c8a65c3479a5f394d0c689b8053
Carl James <carl@jovian34.com>
2024-01-15 12:16:57 -0500

    PT player list in index ATP

--- player_tracking/tests/test_pt_views.py
+++ player_tracking/tests/test_pt_views.py
@@ -1,7 +1,11 @@
 import pytest
 from django.urls import reverse
 
+from player_tracking.tests.fixtures import players
 
-def test_index(client):
+
+@pytest.mark.django_db
+def test_index(client, players):
     response = client.get(reverse("players"))
     assert response.status_code == 200
+    assert "Devin Taylor" in str(response.content)
--- player_tracking/views.py
+++ player_tracking/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from player_tracking.models import Player, Transaction, AnnualRoster
+
 
 def players(request):
-    return render(request, "player_tracking/players.html")
+    players = Player.objects.all()
+    context = {
+        "players": players,
+        "page_title": "players",
+    }
+    return render(request, "player_tracking/players.html", context)

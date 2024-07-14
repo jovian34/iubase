7c3dd11b7fd7b5c5fc2997901732daaf9a97b5f1
Carl James <carl@jovian34.com>
2024-07-14 19:40:24 -0400

    INDEX trying to fix iPhone server error

--- index/models.py
+++ index/models.py
@@ -5,7 +5,7 @@ from django.db.models.functions import Now
 class TrafficCounter(models.Model):
     page = models.CharField(null=False, max_length=128)
     timestamp = models.DateTimeField(db_default=Now())
-    ip = models.CharField(null=True, max_length=128)
+    ip = models.CharField(null=True, max_length=256)
     user_agent = models.CharField(null=True, max_length=128)
 
     def __str__(self) -> str:

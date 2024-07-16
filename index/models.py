57feeefaf38e41dbc2b73cc4df595a132d90b6e3
Carl James <carl@jovian34.com>
2024-07-15 22:20:25 -0400

    traffic counter kill char limit

--- index/models.py
+++ index/models.py
@@ -5,8 +5,8 @@ from django.db.models.functions import Now
 class TrafficCounter(models.Model):
     page = models.CharField(null=False, max_length=128)
     timestamp = models.DateTimeField(db_default=Now())
-    ip = models.CharField(null=True, max_length=256)
-    user_agent = models.CharField(null=True, max_length=256)
+    ip = models.CharField(null=True, max_length=64)
+    user_agent = models.CharField(null=True)
 
     def __str__(self) -> str:
         return f"{self.timestamp}"

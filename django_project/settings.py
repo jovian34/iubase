a07589ed4b4f885416e1f2bf74155a4fea1b847b
Carl James <carl@jovian34.com>
2024-08-11 13:26:21 -0400

    LGB style tweaks, Django 5.1, Python 3.12.5 ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -178,5 +178,5 @@ if not bool(int(os.environ.get("DEVELOP"))):
     STATIC_ROOT = os.path.join(BASE_DIR, "django_project/static/")
 
 
-project_version = "0.0.98"  # PT transfer commits after August start in the second spring ATP 2024-08-02
+project_version = "0.0.99"  # LGB style tweaks, Django 5.1, Python 3.12.5 ATP 2024-08-11
 os.environ.setdefault("PROJECT_VERSION", project_version)

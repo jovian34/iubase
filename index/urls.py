900be12e99d58582ee31bcde76e780fad13369c8
Carl James <carl@jovian34.com>
2024-08-11 12:12:09 -0400

    INDEX - last month traffic report

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from index import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("last_months_traffic", views.last_months_traffic, name="last_months_traffic"),
 ]
--- index/views.py
+++ index/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 from index.models import TrafficCounter
 
@@ -25,3 +27,22 @@ def save_traffic_data(request, page):
 def index(request):              
     save_traffic_data(request, page="Main Index")
     return render(request, "index/index.html")
+
+
+@login_required
+def last_months_traffic(request):
+    first_of_month = datetime(
+        year=datetime.today().year,
+        month=datetime.today().month,
+        day=1,
+        hour=0,
+        minute=0,
+        second=0,
+    )
+    traf = TrafficCounter.objects.filter(timestamp__lt=first_of_month).order_by("-timestamp")
+    context = {
+        "traffic": traf,
+        "count": len(traf),
+        "title": "Last Month's Traffic",
+    }
+    return render(request, "index/last_months_traffic.html", context=context)

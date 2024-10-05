3b6ba43aca71a26bcf9c4177c76cf41ab985ce0e
Carl James <carl@jovian34.com>
2024-10-05 08:33:04 -0400

    DJ-Pro upgrades to Python 3.12.7 and psycopg ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -179,6 +179,6 @@ if not bool(int(os.environ.get("DEVELOP"))):
 
 
 project_version = (
-    "0.1.37"  # ALL same background image gets bigger for media 2024-09-29
+    "0.1.38"  # DJ-Pro upgrades to Python 3.12.7 and psycopg ATP 2024-09-29
 )
 os.environ.setdefault("PROJECT_VERSION", project_version)

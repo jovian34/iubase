6f1d007779ec978df7074e2310a162c2a1dd1592
Carl James <carl@jovian34.com>
2024-11-28 17:07:26 -0500

    added photos to All Players ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -179,6 +179,6 @@ if not bool(int(os.environ.get("DEVELOP"))):
 
 
 project_version = (
-    "0.1.46"  # PT corrected template error ATP 2024-11-28
+    "0.1.47"  # PT added photos to All Players ATP 2024-11-28
 )
 os.environ.setdefault("PROJECT_VERSION", project_version)

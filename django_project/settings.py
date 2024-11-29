f3a4779cd24229879ec891130ef2b3278eeb06dd
Carl James <carl@jovian34.com>
2024-11-28 19:23:50 -0500

    PT all players is a table with more info ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -179,6 +179,6 @@ if not bool(int(os.environ.get("DEVELOP"))):
 
 
 project_version = (
-    "0.1.48"  # PT style updates and upgrade HTMX to 2.0.3 ATP 2024-11-28
+    "0.1.48"  # PT all players is a table with more info ATP 2024-11-28
 )
 os.environ.setdefault("PROJECT_VERSION", project_version)

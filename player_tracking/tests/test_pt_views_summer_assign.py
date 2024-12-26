b94a650e8b283f2a1c9d240ad566b7772fea2af6
Carl James <carl@jovian34.com>
2024-12-25 22:49:07 -0500

    PT accolades included in summer assignments page ATP

--- player_tracking/tests/test_pt_views_summer_assign.py
+++ player_tracking/tests/test_pt_views_summer_assign.py
@@ -8,6 +8,9 @@ from player_tracking.tests.fixtures.summer import (
     summer_teams,
 )
 from player_tracking.tests.fixtures.players import players
+from player_tracking.tests.fixtures.accolades import accolades
+from player_tracking.tests.fixtures.annual_rosters import annual_rosters
+from live_game_blog.tests.fixtures.teams import teams
 
 
 this_year = date.today().year
@@ -20,3 +23,12 @@ def test_summer_assignments_page_renders(
     assert response.status_code == 200
     assert "Devin Taylor" in str(response.content)
     assert "Ryan Kraft" in str(response.content)
+
+
+@pytest.mark.django_db
+def test_summer_assignments_include_accolades(
+    client, players, summer_assign, summer_leagues, summer_teams, accolades
+):
+    response = client.get(reverse("summer_assignments", args=[f"{this_year}"]))
+    assert response.status_code == 200
+    assert "Pitcher of the Year" in str(response.content)
--- player_tracking/views/summer_assignments.py
+++ player_tracking/views/summer_assignments.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models.functions import Lower
 
-from player_tracking.models import SummerAssign
+from player_tracking.models import SummerAssign, Accolade
 from index.views import save_traffic_data
 
 
@@ -9,9 +9,11 @@ def view(request, summer_year):
     assignments = SummerAssign.objects.filter(summer_year=summer_year).order_by(
         Lower("player__last")
     )
+    accolades = Accolade.objects.filter(summer_assign__isnull=False)
     context = {
         "page_title": f"{summer_year} College Summer League Assignments for current and former Indiana players",
         "assignments": assignments,
+        "accolades": accolades,
     }
     save_traffic_data(request=request, page=context["page_title"])
     return render(request, "player_tracking/summer_assignments.html", context)

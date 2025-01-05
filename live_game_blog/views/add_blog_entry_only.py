0a3fe8e1e5d74dac0480968c9cf779f933e2152b
Carl James <carl@jovian34.com>
2025-01-05 15:20:26 -0500

    LGB refactor add blog only manual + black ATP

--- live_game_blog/views/add_blog_entry_only.py
+++ live_game_blog/views/add_blog_entry_only.py
@@ -1,32 +1,31 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django import shortcuts, urls
+from django.contrib.auth import decorators as auth
 
-from accounts.models import CustomUser
-from live_game_blog.forms import BlogEntryForm
-from live_game_blog.models import Game, BlogEntry
+from accounts import models as acc_models
+from live_game_blog import forms as lgb_forms
+from live_game_blog import models as lgb_models
 
 
-@login_required
+@auth.login_required
 def view(request, game_pk):
     if request.method == "POST":
-        form = BlogEntryForm(request.POST)
-        if form.is_valid():
-            add_blog = save_blog_entry(request, game_pk, form)
-            if form.cleaned_data["is_x_embed"]:
-                assign_iubase_as_author(add_blog)
-        return redirect(reverse("edit_live_game_blog", args=[game_pk]))
+        return process_form_validate_and_save_new_blog(request, game_pk)
     else:
-        form = BlogEntryForm()
-        context = {"form": form, "game_pk": game_pk}
-        return render(
-            request, "live_game_blog/partials/add_blog_entry_only.html", context
-        )
+        return render_clean_add_blog_entry_form(request, game_pk)
+
+
+def process_form_validate_and_save_new_blog(request, game_pk):
+    form = lgb_forms.BlogEntryForm(request.POST)
+    if form.is_valid():
+        add_blog = save_blog_entry(request, game_pk, form)
+        if form.cleaned_data["is_x_embed"]:
+            assign_iubase_as_author(add_blog)
+    return shortcuts.redirect(urls.reverse("edit_live_game_blog", args=[game_pk]))
 
 
 def save_blog_entry(request, game_pk, form):
-    add_blog = BlogEntry(
-        game=Game.objects.get(pk=game_pk),
+    add_blog = lgb_models.BlogEntry(
+        game=lgb_models.Game.objects.get(pk=game_pk),
         author=request.user,
         blog_entry=form.cleaned_data["blog_entry"],
         is_raw_html=form.cleaned_data["is_raw_html"],
@@ -37,6 +36,12 @@ def save_blog_entry(request, game_pk, form):
 
 
 def assign_iubase_as_author(add_blog):
-    iubase17 = CustomUser.objects.filter(username="iubase17").last()
+    iubase17 = acc_models.CustomUser.objects.filter(username="iubase17").last()
     add_blog.author = iubase17
     add_blog.save()
+
+
+def render_clean_add_blog_entry_form(request, game_pk):
+    context = {"form": lgb_forms.BlogEntryForm(), "game_pk": game_pk}
+    template_path = "live_game_blog/partials/add_blog_entry_only.html"
+    return shortcuts.render(request, template_path, context)

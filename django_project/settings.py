712ab1a4772a9fe64b8b0991d761e4a2865aa4d8
Carl James <carl@jovian34.com>
2025-01-05 18:22:52 -0500

    LGB add team refactoring manual and black ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -165,6 +165,6 @@ if bool(int(os.environ.get("DEVELOP"))):
     SECURE_SSL_REDIRECT = False
 
 project_version = (
-    "2025.01.05.b"  # LGB refactoring manual and black ATP
+    "2025.01.05.c"  # LGB add team refactoring manual and black ATP
 )
 os.environ.setdefault("PROJECT_VERSION", project_version)
--- live_game_blog/views/add_team.py
+++ live_game_blog/views/add_team.py
@@ -1,29 +1,40 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django import shortcuts, urls
+from django.contrib.auth import decorators as auth
 
-from live_game_blog.forms import AddTeamForm
-from live_game_blog.models import Team
+from live_game_blog import forms as lgb_forms
+from live_game_blog import models as lgb_models
 
 
-@login_required
+@auth.login_required
 def view(request):
     if request.method == "POST":
-        form = AddTeamForm(request.POST)
-        if form.is_valid():
-            add_team = Team(
-                team_name=form.cleaned_data["team_name"],
-                mascot=form.cleaned_data["mascot"],
-                logo=form.cleaned_data["logo"],
-                stats=form.cleaned_data["stats"],
-                roster=form.cleaned_data["roster"],
-            )
-            add_team.save()
-        return redirect(reverse("games"))
+        return validate_posted_add_team_form_save_then_redirect(request)
     else:
-        form = AddTeamForm()
-        context = {
-            "form": form,
-            "page_title": "Add Team",
-        }
-        return render(request, "live_game_blog/add_team.html", context)
+        return initial_blank_add_team_form_and_render_template(request)
+
+
+def initial_blank_add_team_form_and_render_template(request):
+    context = {
+        "form": lgb_forms.AddTeamForm(),
+        "page_title": "Add Team",
+    }
+    template_path = "live_game_blog/add_team.html"
+    return shortcuts.render(request, template_path, context)
+
+
+def validate_posted_add_team_form_save_then_redirect(request):
+    form = lgb_forms.AddTeamForm(request.POST)
+    if form.is_valid():
+        save_team(form)
+    return shortcuts.redirect(urls.reverse("games"))
+
+
+def save_team(form):
+    add_team = lgb_models.Team(
+        team_name=form.cleaned_data["team_name"],
+        mascot=form.cleaned_data["mascot"],
+        logo=form.cleaned_data["logo"],
+        stats=form.cleaned_data["stats"],
+        roster=form.cleaned_data["roster"],
+    )
+    add_team.save()

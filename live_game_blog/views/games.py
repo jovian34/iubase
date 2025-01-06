f9c2592f867a01ecadb868e66f18ae605279f0c0
Carl James <carl@jovian34.com>
2025-01-05 19:56:28 -0500

    LGB games refactoring manual and black ATP

--- live_game_blog/views/games.py
+++ live_game_blog/views/games.py
@@ -1,34 +1,41 @@
-from django.shortcuts import render, redirect
-from django.db.models import Q
+from django import shortcuts, urls
+from django.db import models as db_models
 
 from index.views import save_traffic_data
-from live_game_blog.models import Game, Scoreboard
+from live_game_blog import models as lgb_models
 
 
 def upcoming(request):
-    finals = Scoreboard.objects.filter(
-        Q(game_status="final") | Q(game_status="cancelled") | Q(game_status="post-game")
+    context = get_upcoming_games()
+    template_path = "live_game_blog/games.html"
+    save_traffic_data(request=request, page=context["page_title"])
+    return shortcuts.render(request, template_path, context)
+
+
+def get_upcoming_games():
+    finals = lgb_models.Scoreboard.objects.filter(
+        db_models.Q(game_status="final")
+        | db_models.Q(game_status="cancelled")
+        | db_models.Q(game_status="post-game")
     )
     final_pks = [final.game.pk for final in finals]
-    games = Game.objects.exclude(pk__in=final_pks).order_by("first_pitch")[:3]
     context = {
         "page_title": "iubase.com Live Game Blog Games",
-        "games": games,
+        "games": lgb_models.Game.objects.exclude(pk__in=final_pks).order_by(
+            "first_pitch"
+        )[:3],
     }
-    save_traffic_data(request=request, page=context["page_title"])
-    return render(request, "live_game_blog/games.html", context)
+    return context
 
 
 def past(request):
     if request.META.get("HTTP_HX_REQUEST"):
-        scoreboards = (
-            Scoreboard.objects.select_related("game")
-            .filter(game_status="final")
-            .order_by("-update_time")
-        )
         context = {
-            "scoreboards": scoreboards,
+            "scoreboards": lgb_models.Scoreboard.objects.select_related("game")
+            .filter(game_status="final")
+            .order_by("-update_time"),
         }
-        return render(request, "live_game_blog/partials/past_games.html", context)
+        template_path = "live_game_blog/partials/past_games.html"
+        return shortcuts.render(request, template_path, context)
     else:
-        return redirect("games")
+        return shortcuts.redirect(urls.reverse("games"))

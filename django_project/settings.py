23a7b9a4e49b348561d79654bc424df75a24c9c7
Carl James <carl@jovian34.com>
2025-01-05 20:09:02 -0500

    LGB live_game_blog refactoring manual and black ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -165,6 +165,6 @@ if bool(int(os.environ.get("DEVELOP"))):
     SECURE_SSL_REDIRECT = False
 
 project_version = (
-    "2025.01.05.e"  # LGB games refactoring manual and black ATP
+    "2025.01.05.f"  # LGB live_game_blog refactoring manual and black ATP
 )
 os.environ.setdefault("PROJECT_VERSION", project_version)
--- live_game_blog/views/live_game_blog.py
+++ live_game_blog/views/live_game_blog.py
@@ -1,25 +1,33 @@
-from django.shortcuts import render
+from django import shortcuts
 from django import http
 
 from index.views import save_traffic_data
-from live_game_blog.models import Game, Scoreboard, BlogEntry
+from live_game_blog import models as lgb_models
 
 
 def view(request, game_pk):
-    try:
-        game = Game.objects.get(pk=game_pk)
-    except Game.DoesNotExist:
-        raise http.Http404
-    blog_entries = (
-        BlogEntry.objects.filter(game=game)
-        .select_related("scoreboard")
-        .order_by("-blog_time")
-    )
-    last_score = Scoreboard.objects.filter(game=game).order_by("-update_time")[0]
     context = {
-        "entries": blog_entries,
-        "game": game,
-        "last_score": last_score,
+        "game": get_game_or_raise_404(game_pk),
+        "entries": get_blog_entries(game_pk),
+        "last_score": lgb_models.Scoreboard.objects.filter(game=game_pk).order_by(
+            "-update_time"
+        )[0],
     }
-    save_traffic_data(request=request, page=game.__str__())
-    return render(request, "live_game_blog/live_game_blog.html", context)
+    template_path = "live_game_blog/live_game_blog.html"
+    save_traffic_data(request=request, page=context["game"].__str__())
+    return shortcuts.render(request, template_path, context)
+
+
+def get_blog_entries(game_pk):
+    blog_entries = lgb_models.BlogEntry.objects.filter(game=game_pk).order_by(
+        "-blog_time"
+    )
+    return blog_entries
+
+
+def get_game_or_raise_404(game_pk):
+    try:
+        game = lgb_models.Game.objects.get(pk=game_pk)
+    except lgb_models.Game.DoesNotExist:
+        raise http.Http404
+    return game

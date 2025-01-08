5df88baa532b7911cb4d852bc31276baf76e73b7
Carl James <carl@jovian34.com>
2025-01-08 16:01:48 -0500

    PT template tweaks ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -165,6 +165,6 @@ if bool(int(os.environ.get("DEVELOP"))):
     SECURE_SSL_REDIRECT = False
 
 project_version = (
-    "2025.01.06.a"  # ACCOUNTS fixed fieldset bug in add user via admin ATP
+    "2025.01.08.a"  # PT template tweaks ATP
 )
 os.environ.setdefault("PROJECT_VERSION", project_version)

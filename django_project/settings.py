cb4efa3f7fa90b19a1cbb814f63f1582a33269bf
Carl James <carl@jovian34.com>
2025-01-11 11:17:55 -0500

    CSS added a 4K media query ATP

--- django_project/settings.py
+++ django_project/settings.py
@@ -165,6 +165,6 @@ if bool(int(os.environ.get("DEVELOP"))):
     SECURE_SSL_REDIRECT = False
 
 project_version = (
-    "2025.01.09.c"  # PT spring roster shows accolades under each player ATP
+    "2025.01.11.a"  # CSS added a 4K media query ATP
 )
 os.environ.setdefault("PROJECT_VERSION", project_version)

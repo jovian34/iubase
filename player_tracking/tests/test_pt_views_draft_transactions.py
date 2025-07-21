e2b6e46ff0bf8e850174beb7c11f085791f050c6
Carl James <carl@jovian34.com>
2025-07-21 17:39:53 -0400

    created two xfail tests

---
+++ player_tracking/tests/test_pt_views_draft_transactions.py
@@ -0,0 +1,25 @@
+import pytest
+from django import urls
+
+from player_tracking.tests.fixtures.annual_rosters import annual_rosters
+from player_tracking.tests.fixtures.mlb_draft_date import typical_mlb_draft_date
+from player_tracking.tests.fixtures.players import players
+from player_tracking.tests.fixtures.prof_org import prof_orgs
+from player_tracking.tests.fixtures.transactions import transactions
+from player_tracking.views import drafted_players
+from player_tracking.views import set_player_properties
+from live_game_blog.tests.fixtures.teams import teams
+
+
+@pytest.mark.xfail
+@pytest.mark.django_db
+def test_group_draft_transactions_render(client):
+    response = client.get(urls.reverse("draft_transactions"))
+    assert response.status_code == 200
+
+
+@pytest.mark.xfail
+@pytest.mark.django_db
+def test_group_draft_transactions_list_player(client):
+    response = client.get(urls.reverse("draft_transactions"))
+    assert "Nick Mitchell" in str(response.content)

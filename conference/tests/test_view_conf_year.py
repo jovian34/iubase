ca0d83ae07d2abd4486f1e875f6d0322d657a27c
Carl James <carl@jovian34.com>
2025-08-09 14:35:17 -0400

    CONF - teams by year ordered by team name ATP

--- conference/tests/test_view_conf_year.py
+++ conference/tests/test_view_conf_year.py
@@ -35,3 +35,18 @@ def test_last_year_b1g_does_not_show_ucla(client, teams, conferences, conf_teams
     assert "UCLA" not in str(response.content)
     assert "Indiana" in str(response.content)
     assert "Iowa" in str(response.content)
+
+
+
+@pytest.mark.django_db
+def test_current_year_b1g_shows_correct_member_order(client, teams, conferences, conf_teams):
+    response = client.get(
+        urls.reverse(
+            "conf_year", 
+            kwargs = {"conf": "B1G", "spring_year": str(date.today().year)}
+        )
+    )
+    output = str(response.content)
+    ucla = output.find("UCLA")
+    iu = output.find("Indiana")
+    assert ucla > iu

70b2944c9fdc24dbb274879659d5d3b0fa62cb7b
Carl James <carl@jovian34.com>
2025-11-30 18:44:48 -0500

    LGB required perms for add stadium page - ATP

--- live_game_blog/tests/test_test_lgb_views_stadiums.py
+++ live_game_blog/tests/test_test_lgb_views_stadiums.py
@@ -5,17 +5,31 @@ from live_game_blog.tests.fixtures.teams import teams
 from live_game_blog.tests.fixtures.home_stadium import home_stadium
 from live_game_blog.tests.fixtures.stadium_configs import stadium_configs
 from live_game_blog.tests.fixtures.stadiums import stadiums
+from live_game_blog.tests.fixtures.games import logged_user_schwarbs
 
-def test_stadium_page_renders(client):
+
+@pytest.mark.django_db
+def test_stadium_page_fails_wo_perms(client, logged_user_schwarbs):
     response = client.get(urls.reverse("stadiums"))
+    assert response.status_code == 403
+
+
+def test_stadium_page_renders(admin_client):
+    response = admin_client.get(urls.reverse("stadiums"))
     assert response.status_code == 200
     assert "Stadium Actions" in str(response.content)
     assert "get teams without stadium configuration"
 
 
 @pytest.mark.django_db
-def test_teams_wo_stad_config_partial_renders_correct_teams(client, teams, home_stadium, stadium_configs, stadiums):
+def test_teams_wo_stad_config_partial_fails_wo_perms(client, logged_user_schwarbs):
     response = client.get(urls.reverse("teams_wo_stad_config"))
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_teams_wo_stad_config_partial_renders_correct_teams(admin_client, teams, home_stadium, stadium_configs, stadiums):
+    response = admin_client.get(urls.reverse("teams_wo_stad_config"))
     assert response.status_code == 200
     assert "Kentucky" in str(response.content)
     assert "Indiana" not in str(response.content)
--- live_game_blog/views/stadiums.py
+++ live_game_blog/views/stadiums.py
@@ -1,23 +1,29 @@
-from django import shortcuts
+from django import shortcuts, http
 from django.db.models import Q
 
 from live_game_blog import models as lgb_models
 from conference import models as conf_models
 
 def view(request):
-    template_path = "live_game_blog/stadiums.html"
-    context = {
-        "title": "Stadium Actions",
-    }
-    return shortcuts.render(request, template_path, context)
+    if not request.user.has_perm("live_game_blog.add_stadium"):
+        return http.HttpResponseForbidden()
+    else:
+        template_path = "live_game_blog/stadiums.html"
+        context = {
+            "title": "Stadium Actions",
+        }
+        return shortcuts.render(request, template_path, context)
 
 
 def teams_wo_stad_config(request):
-    teams = lgb_models.Team.objects.filter(
-        Q(homestadium__isnull=True)
-    ).order_by("team_name")
-    template_path = "live_game_blog/partials/teams_wo_stad_config.html"
-    context = {
-        "teams": teams,
-    }
-    return shortcuts.render(request, template_path, context)
+    if not request.user.has_perm("live_game_blog.add_stadium"):
+        return http.HttpResponseForbidden()
+    else:
+        teams = lgb_models.Team.objects.filter(
+            Q(homestadium__isnull=True)
+        ).order_by("team_name")
+        template_path = "live_game_blog/partials/teams_wo_stad_config.html"
+        context = {
+            "teams": teams,
+        }
+        return shortcuts.render(request, template_path, context)

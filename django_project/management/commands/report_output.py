93c94b3ca8c6d7d28624ec25e94a2134b62630da
Carl James <carl@jovian34.com>
2025-12-22 16:24:41 -0500

    added a sample manage command -ATP

---
+++ django_project/management/commands/report_output.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from live_game_blog import models as lgb_models
+
+
+class Command(BaseCommand):
+    help = "report a database output to prove this works"
+
+    def add_arguments(self, parser):
+        return super().add_arguments(parser)
+    
+    def handle(self, *args, **options):
+        iu = lgb_models.Team.objects.get(team_name="Indiana")
+        self.stdout.write(
+            f"Indiana is the {iu.mascot}"
+        )

e0e70184126474a535507dd650add4ae18fa4bc3
Carl James <carl@jovian34.com>
2025-12-23 10:21:14 -0500

    LGB call weather daily from manage.py cmd - ATP

---
+++ django_project/management/commands/weather_daily.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+from live_game_blog import weather
+
+
+class Command(BaseCommand):
+    help = "set weather data for games 8 to 500 days out"
+
+    def add_arguments(self, parser):
+        return super().add_arguments(parser)
+    
+    def handle(self, *args, **options):
+        weather.get_weather_for_games_over_one_week_from_now()

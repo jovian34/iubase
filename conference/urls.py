3452b177c22538cebc950593a101eef7654568a1
Carl James <carl@jovian34.com>
2025-12-28 18:35:25 -0500

    CONF fixed url pattern order bug that showed up when I added a third pattern by adding a prefix ATP

--- conference/urls.py
+++ conference/urls.py
@@ -4,7 +4,7 @@ from conference.views import conf_year, add_series
 
 
 urlpatterns = [
-    urls.path("<conf>/<spring_year>/", conf_year.view, name="conf_year"),
-    urls.path("<conf>/", conf_year.view, name="conf_year_default"),
-    urls.path("add_series", add_series.view, name="add_series"),
+    urls.path("members/<conf>/<spring_year>/", conf_year.view, name="conf_year"),
+    urls.path("members/<conf>/", conf_year.view, name="conf_year_default"),
+    urls.path("add_series/", add_series.view, name="add_series"),
 ]

f01959f4312386e7f5ff4c14be220129acc628f2
Carl James <carl@jovian34.com>
2025-12-28 19:09:05 -0500

    CONF - add serires page renders with title only ATP

--- conference/tests/test_view_conf_add_series.py
+++ conference/tests/test_view_conf_add_series.py
@@ -12,3 +12,10 @@ if datetime.date.today().month > 8:
 def test_add_series_get_renders(admin_client):
     response = admin_client.get(urls.reverse("add_series", args=[spring_year]))
     assert response.status_code == 200
+    assert f"Add {spring_year} Conference Series" in response.content.decode()
+
+
+@pytest.mark.django_db
+def test_add_series_get_is_forbidden_without_perms(client):
+    response = client.get(urls.reverse("add_series", args=[spring_year]))
+    assert response.status_code == 403
--- conference/views/add_series.py
+++ conference/views/add_series.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django import shortcuts
+from django import shortcuts, http
 
 spring_year = datetime.date.today().year
 if datetime.date.today().month > 8:
@@ -8,8 +8,10 @@ if datetime.date.today().month > 8:
 
 
 def view(request, spring_year=spring_year):
+    if not request.user.has_perm("conference.add_confseries"):
+        return http.HttpResponseForbidden()
     template_path = "conference/add_series.html",
     context = {
-
+        "page_title": f"Add {spring_year} Conference Series",
     }
     return shortcuts.render(request, template_path, context)

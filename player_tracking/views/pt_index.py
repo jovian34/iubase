b70106420d77bf27e45130f3b0ad43c62d40efd2
Carl James <carl@jovian34.com>
2026-01-08 08:48:08 -0500

    PT simplified variables in index ATP

--- player_tracking/views/pt_index.py
+++ player_tracking/views/pt_index.py
@@ -5,11 +5,7 @@ from index.views import save_traffic_data
 
 
 def view(request):
-    current_spring = date.today().year
-    current_fall = current_spring - 1
     context = {
-        "fall": current_fall,
-        "spring": current_spring,
         "page_title": "Player Tracking",
         "this_year": str(date.today().year),
         "last_year": str(date.today().year - 1),

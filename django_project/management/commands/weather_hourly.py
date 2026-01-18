594f423d850fe8c22a05ede19194446576d3cc1f
Carl James <carl@jovian34.com>
2026-01-18 16:16:14 -0500

    DJP set weather hourly command ATP

---
+++ django_project/management/commands/weather_hourly.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+from live_game_blog.logic import weather_hourly
+
+
+class Command(BaseCommand):
+    help = "set weather data for games under 48 hours out"
+
+    def add_arguments(self, parser):
+        return super().add_arguments(parser)
+    
+    def handle(self, *args, **options):
+        weather_hourly.get_and_set_weather_data_hourly()

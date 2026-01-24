5a2d02b962e8cb4cc2574e2042715bec7dad31b6
Carl James <carl@jovian34.com>
2026-01-24 18:34:31 -0500

    CONF test HTMX request check to add series wins ATP

--- conference/tests/test_view_add_wins.py
+++ conference/tests/test_view_add_wins.py
@@ -13,7 +13,10 @@ from conference import models as conf_models
 
 @pytest.mark.django_db
 def test_add_away_win_increases_win_by_one(admin_client, conferences, conf_teams, conf_series_current):
-    response = admin_client.get(urls.reverse("add_away_win", args=[conf_series_current.iu_iowa.pk]))
+    response = admin_client.get(
+        urls.reverse("add_away_win", args=[conf_series_current.iu_iowa.pk]),
+        HTTP_HX_REQUEST="true",
+    )
     series = conf_models.ConfSeries.objects.get(pk=conf_series_current.iu_iowa.pk)
     assert series.away_wins == 1.0
     assert series.home_wins == 0
@@ -21,15 +24,32 @@ def test_add_away_win_increases_win_by_one(admin_client, conferences, conf_teams
 
 @pytest.mark.django_db
 def test_add_away_win_does_not_make_changes_without_perms(client, conferences, conf_teams, conf_series_current):
-    response = client.get(urls.reverse("add_away_win", args=[conf_series_current.iu_iowa.pk]))
+    response = client.get(
+        urls.reverse("add_away_win", args=[conf_series_current.iu_iowa.pk]),
+        HTTP_HX_REQUEST="true",
+    )
     series = conf_models.ConfSeries.objects.get(pk=conf_series_current.iu_iowa.pk)
     assert series.away_wins == 0
     assert series.home_wins == 0
 
 
 @pytest.mark.django_db
+def test_add_away_win_does_not_make_changes_without_htmx_request(admin_client, conferences, conf_teams, conf_series_current):
+    response = admin_client.get(
+        urls.reverse("add_away_win", args=[conf_series_current.iu_iowa.pk]),
+        follow=True,
+    )
+    series = conf_models.ConfSeries.objects.get(pk=conf_series_current.iu_iowa.pk)
+    assert series.away_wins == 0
+    assert series.home_wins == 0
+    
+
+@pytest.mark.django_db
 def test_add_home_win_increases_win_by_one(admin_client, conferences, conf_teams, conf_series_current):
-    response = admin_client.get(urls.reverse("add_home_win", args=[conf_series_current.iu_iowa.pk]))
+    response = admin_client.get(
+        urls.reverse("add_home_win", args=[conf_series_current.iu_iowa.pk]),
+        HTTP_HX_REQUEST="true",
+    )
     series = conf_models.ConfSeries.objects.get(pk=conf_series_current.iu_iowa.pk)
     assert series.home_wins == 1.0
     assert series.away_wins == 0
@@ -37,7 +57,9 @@ def test_add_home_win_increases_win_by_one(admin_client, conferences, conf_teams
 
 @pytest.mark.django_db
 def test_add_home_win_does_not_make_changes_without_perms(client, conferences, conf_teams, conf_series_current):
-    response = client.get(urls.reverse("add_home_win", args=[conf_series_current.iu_iowa.pk]))
+    response = client.get(
+        urls.reverse("add_home_win", args=[conf_series_current.iu_iowa.pk]),
+        HTTP_HX_REQUEST="true",)
     series = conf_models.ConfSeries.objects.get(pk=conf_series_current.iu_iowa.pk)
     assert series.home_wins == 0
     assert series.away_wins == 0
@@ -45,7 +67,10 @@ def test_add_home_win_does_not_make_changes_without_perms(client, conferences, c
 
 @pytest.mark.django_db
 def test_add_tie_increases_wins_by_each_by_half(admin_client, conferences, conf_teams, conf_series_current):
-    response = admin_client.get(urls.reverse("add_tie", args=[conf_series_current.iu_iowa.pk]))
+    response = admin_client.get(
+        urls.reverse("add_tie", args=[conf_series_current.iu_iowa.pk]),
+        HTTP_HX_REQUEST="true",
+    )
     series = conf_models.ConfSeries.objects.get(pk=conf_series_current.iu_iowa.pk)
     assert series.home_wins == 0.5
     assert series.away_wins == 0.5
